f2b10dad46797e0a76e3e813234e03a32964f645
Nathalya Muniz <pcnathalya@hotmail.com>
2021-04-20 20:54:42 -0300

    add all level buttons

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:devquiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +13,28 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBarWidget(),
-    );
+        appBar: AppBarWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal:
+                  19.8), // ajustar o correto é 20 mas está transbordando
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              LevelButtonWidget(
+                label: "Fácil",
+              ),
+              LevelButtonWidget(
+                label: "Médio",
+              ),
+              LevelButtonWidget(
+                label: "Difícil",
+              ),
+              LevelButtonWidget(
+                label: "Perito",
+              ),
+            ],
+          ),
+        ));
   }
 }
